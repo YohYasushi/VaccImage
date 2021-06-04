c73fecc83e6060cd111fa96ea629cb640f60140b
YohYasushi <72450594+YohYasushi@users.noreply.github.com>
2021-06-04 13:11:35 +0900

    Add files via upload

---
+++ vaccimage/vaccimage1.0.1/sql/allmember.sql
@@ -0,0 +1,10 @@
+CREATE TABLE allmember (
+
+ptid   INT NOT NULL,
+last_name  VARCHAR(60) NOT NULL,
+first_name  VARCHAR(60) NOT NULL,
+birthday DATE NOT NULL, 
+department VARCHAR(100),
+PRIMARY KEY(ptid)
+
+);
